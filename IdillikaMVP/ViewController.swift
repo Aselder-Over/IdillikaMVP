cf8593334103e7cfa14214fcb7fbe45fa6b0fafd
Aselder <asover98@gmail.com>
2020-08-05 13:37:17 +0300

    Initial Commit

---
+++ IdillikaMVP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IdillikaMVP
+//
+//  Created by Асельдер on 05.08.2020.
+//  Copyright © 2020 Асельдер. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
